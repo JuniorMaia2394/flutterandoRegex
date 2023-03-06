efd044ec9067aa03f6ac77cda30d4139b03b0d6a
Junior Maia <maiajr2394@gmail.com>
2023-03-06 08:33:51 -0300

    dart_regex

---
+++ bin/dart_regex.dart
@@ -0,0 +1,5 @@
+import 'package:dart_regex/dart_regex.dart' as dart_regex;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_regex.calculate()}!');
+}
---
+++ test/dart_regex_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_regex/dart_regex.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
